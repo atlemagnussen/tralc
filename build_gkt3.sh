e9148f0cdcff89d3848f3923d66944674ebbd8c9
Atle Magnussen <atlemagnussen@gmail.com>
2019-12-30 10:19:51 +0100

    try trans
    no exe in git

--- build_gkt3.sh
+++ build_gkt3.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
-cc `pkg-config --cflags gtk+-3.0` -o tralc src/main.c `pkg-config --libs gtk+-3.0`
+cc `pkg-config --cflags gtk+-3.0` -o tralc-test src/test.c `pkg-config --libs gtk+-3.0`
 
